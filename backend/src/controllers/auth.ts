cd9e3aa011167f59951eeb00486530a889fde6f9
Sumanyu301 <sumanyukodan301@gmail.com>
2025-05-02 17:22:37 +0530

    added the login and logout logic in the controller

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -10,6 +10,11 @@ const signupSchema = z.object({
     password: z.string().min(6),
 });
 
+const loginSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+});
+
 export const signup = async (req: Request, res: Response): Promise<void> => {
     const {fullName, email, password} = req.body;
     try {
@@ -45,15 +50,65 @@ export const signup = async (req: Request, res: Response): Promise<void> => {
             });
         }
     }
-    catch(e) {
+    catch(error) {
         res.status(500).json({ error: "Internal server error" });
     }
 };
 
 export const login = async (req: Request, res: Response): Promise<void> => {
-    res.send("login route");
+    const {email,password} = req.body;
+    try{
+        const parsedData = loginSchema.safeParse({email, password});
+        if (!parsedData.success) {
+            res.status(400).json({
+                error: parsedData.error.errors.map((error) => error.message),
+            });
+            return;
+        }
+
+        const user = await User.findOne({email});
+        if(!user || !user.password)
+        {
+            res.status(400).json({
+                message:"Invalid credentials"
+            });
+            return;
+        }
+        const correctPass = await bcrypt.compare(password,user.password);
+        if(!correctPass)
+        {
+            res.status(400).json({
+                message:"Invalid credentials"
+            });
+            return;
+        }
+        generateToken(user._id,res);
+        res.status(200).json({
+                _id: user._id,
+                fullName: user.fullName,
+                email: user.email,
+                profilePic: user.profilePic
+        })
+    }
+    catch(error){
+        console.log("err in login controller");
+        res.status(500).json({
+            message:"internal server error"
+        });
+    }
 };
 
 export const logout = async (req: Request, res: Response): Promise<void> => {
-    res.send("logout route");
+    try{
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({
+            message:"logged out successfully"
+        });
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({
+            message:"internal server error"
+        });
+    }
 };
