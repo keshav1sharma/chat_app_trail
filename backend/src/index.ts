2849b39003f2f8ca22a36e94fc28b1eaa1bcf875
Sumanyu301 <sumanyukodan301@gmail.com>
2025-05-06 04:04:03 +0530

    refactor: Clean up commented code and update package scripts for better clarity

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,14 +4,14 @@ import messageRoutes from "./routes/message";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
 import cors from "cors";
-import path from "path";
+// import path from "path";
 
 import {app,server} from "./lib/socket";
 import { connectDb } from "./lib/db";
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 // Increase payload size limits
 app.use(express.json({ limit: '50mb' }));
@@ -38,12 +38,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get("*", (req: Request, res: Response) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-    });
-}
+// if(process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//     app.get("*", (req: Request, res: Response) => {
+//         res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+//     });
+// }
 
 server.listen(PORT,()=>{
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
